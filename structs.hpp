904326e8c2397370254c485075565f15ed75f99d
hushed <vhushed@users.noreply.github.com>
2026-05-27 15:47:09 +0800

    base structs.hpp

---
+++ structs.hpp
@@ -0,0 +1,17 @@
+#ifndef STRUCTS_HPP
+#define STRUCTS_HPP
+#include <string>
+#include <iostream>
+
+struct Order {
+    int orderID;
+    std::string itemName;
+    int itemID;
+    std::string status;  
+    // status enumerate: PENDING, PROCESSING, COMPLETED 
+    Order* next;    
+    Order(int id, std::string iName, int iID) 
+        : orderID(id), itemName(iName), itemID(iID), status("PENDING"), next(nullptr) {}
+};
+
+#endif
